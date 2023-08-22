e0f0527553565e2be75d1edcf6856b75826d8894
techp0rtal <jlundstedtiii@gmail.com>
2023-08-22 00:01:22 -0400

    Commit 2.3

--- main.py
+++ main.py
@@ -14,7 +14,18 @@ big_font = pygame.font.Font('freesansbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
 # game variables and images
-white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+white_locations = [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0),
+                   (0,1), (1,1), (2,1), (3,1), (4,1), (5,1), (6,1), (7,1)]
+
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+black_locations = [(0,7), (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7),
+                   (0,6), (1,6), (2,6), (3,6), (4,6), (5,6), (6,6), (7,6)]
+
+
+
 #main game loop
 run = True
 while run:
