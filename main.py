a64fdb7315ee961a00f5ff0f801b5da309304a94
techp0rtal <jlundstedtiii@gmail.com>
2023-09-05 22:19:47 -0400

    Commit 2.4

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight',
                 'pawn', 'pawn','pawn','pawn','pawn','pawn','pawn','pawn']
 black_locations = [(0,7), (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7),
                    (0,6), (1,6), (2,6), (3,6), (4,6), (5,6), (6,6), (7,6)]
+captured_pieces_white = []
+captured_pieces_black = []
 
 
 #main game loop
